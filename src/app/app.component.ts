e04299790648078beaa62bc8d6d2cec52d37b02c
fangplay <fangplay_2014@hotmail.com>
2024-03-23 18:51:27 +0700

    index update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-front-project';
+  title = 'SPECTRUM Next Angular Beta';
 }
